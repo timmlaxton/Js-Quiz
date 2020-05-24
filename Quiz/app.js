83fa570a0db877fe4b110cfe561d99940cd80c88
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-24 11:45:34 +0100

    added animation to score

--- Quiz/app.js
+++ Quiz/app.js
@@ -17,7 +17,17 @@ form.addEventListener('submit', e => {
 
     // render results to screen
     scrollTo(0,0);
-    result.querySelector('span').textContent = `${score}%`;
+    
     result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        }else{
+           output++; 
+        }
+        }, 10);
 });
 
